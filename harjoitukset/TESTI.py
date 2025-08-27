c186aa8cb64bdf532f5a8dff44ef7dc74b5268be
Jicoi <jico.isola@metropolia.fi>
2025-08-27 19:52:52 +0300

    Update TESTI.py

--- harjoitukset/TESTI.py
+++ harjoitukset/TESTI.py
@@ -3,6 +3,7 @@ lname = input("LAST NAME: ")
 
 carBrand = input("car brand?: ")
 
+#Testinä printtaa kuvitteellisen autosi huollon päivän huomiselle
 
 print("HELLO "  + user , lname + ", HAVE A NICE DAY!")
 print("your maintenance for\n ... " + carBrand + " is tomorrow at 17:00" )
