6dcc9a052cd18c906a974cbb46b06355514f6cbc
Jigis <jico.isola@metropolia.fi>
2025-11-01 00:27:32 +0200

    Valmis

--- Tehtavat/Moduuli 6/Teht6.2.py
+++ Tehtavat/Moduuli 6/Teht6.2.py
@@ -5,17 +5,22 @@ import random
 # Kysytään käyttäjältä arvo
 maksimi = input("Anna maksimi arvo: ")
 
+
 def noppa(maksimi):
     return random.randint(1, maksimi)       # haetaan arvo 1 - käyttäjän syöttämän arvon väliltä.
 
 
-def kutonen(noppa):
+def kutonen(noppa, kierrokset):
     print(f"Heitto: {noppa}")
     if int(noppa) == int(maksimi):             # pysäyttää funktion kun ollaan saatu käyttäjän syöttämä arvo
-        print(f"Maksimisi saatu!: {maksimi}")
+        print(f"Maksimisi saatu!: {maksimi}. Sinulla meni {kierrokset} kierrosta")
         exit()
 
+kierrokset = 0
 
 while True:
-    kutonen(noppa(int(maksimi)))                # kutsutaan funktiota kutonen ja sen sisään annetaan -
+    kierrokset += 1
+    kutonen(noppa(int(maksimi)), kierrokset)                # kutsutaan funktiota kutonen ja sen sisään annetaan -
                                                 # - funktio noppa, joka käyttää arvoa maksimi
+
+    # testi mielessä kokeilin lisätä laskurin heitetyille kierroksille.
